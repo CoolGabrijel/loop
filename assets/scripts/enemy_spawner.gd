d578a25d93e019d87b29df962f4edb9b7cac5c9a
CoolGabrijel <46607201+CoolGabrijel@users.noreply.github.com>
2025-08-01 13:26:21 +0200

    Added Spawning Enemies

--- assets/scripts/enemy_spawner.gd
+++ assets/scripts/enemy_spawner.gd
@@ -3,9 +3,14 @@ class_name EnemySpawner
 
 const CHASER_ENEMY = preload("res://scenes/characters/enemies/chaser/chaser_enemy.tscn")
 
+@export var patrol_waypoints : Node3D
+
 func _ready() -> void:
+	spawn_enemy()
 	pass
 
 func spawn_enemy() -> void:
-	var enemy := CHASER_ENEMY.instantiate()
+	var enemy: chaser_enemy = CHASER_ENEMY.instantiate()
+	enemy.patrol_points = patrol_waypoints.get_path()
+	enemy.player_ref = Player.player
 	add_child(enemy)
