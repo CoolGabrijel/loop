0d0a8cdc02c60466514c07a2aa58b0b93b1cd00c
Kusaku <kusakot@gmail.com>
2025-08-01 14:47:52 +0300

    temporaty pause menu and quit implemented

--- assets/scripts/main_menu.gd
+++ assets/scripts/main_menu.gd
@@ -6,5 +6,15 @@ extends CanvasLayer
 
 @onready var main_menu: CanvasLayer = $"."
 
-func _on_play_button_pressed() -> void:
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.keycode == KEY_ESCAPE and event.is_pressed():
+			main_menu.show()
+
+
+func _on_play_button_pressed():
 	main_menu.hide()
+
+
+func _on_quit_game_button_pressed() -> void:
+	get_tree().quit()
