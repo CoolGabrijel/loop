43f8195ed0a33395f19a513c7c0dabe411da7d36
Kusaku <kusakot@gmail.com>
2025-08-01 16:55:46 +0300

    made a credits scene

--- assets/scripts/main_menu.gd
+++ assets/scripts/main_menu.gd
@@ -3,11 +3,13 @@ extends CanvasLayer
 @onready var play_button: Button = $ButtonHolder/PlayButton
 @onready var settings_button: Button = $ButtonHolder/SettingsButton
 @onready var quit_game_button: Button = $ButtonHolder/QuitGameButton
+@onready var credits_button: Button = $CreditsButton
 
 var pause_menu_is_showing: bool
 
 signal play_button_was_pressed
 signal options_button_was_pressed
+signal credits_button_was_pressed
 
 func _on_play_button_pressed() -> void:
 	hide()
@@ -20,3 +22,11 @@ func _on_settings_button_pressed() -> void:
 
 func _on_quit_game_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_mute_button_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+
+
+func _on_credits_button_pressed() -> void:
+	emit_signal("credits_button_was_pressed")
