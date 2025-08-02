7f4136f70fd58b3d095ddcff5f3b6c44037e2b23
CoolGabrijel <46607201+CoolGabrijel@users.noreply.github.com>
2025-08-02 13:32:32 +0200

    Fixed health pickup

--- assets/scripts/pickups/pickup_health.gd
+++ assets/scripts/pickups/pickup_health.gd
@@ -1,9 +1,15 @@
 extends Area3D
 
-@onready var player: Player = $Player
-
+var has_been_picked_up := false
 
 func _on_body_entered(body: Node3D) -> void:
-	player.health.heal(5)
-	print("pickup was picked, health regained!")
+	if body is not Player:
+		return
+	
+	if has_been_picked_up:
+		return
+	
+	Player.player.health_node.heal(5)
+	has_been_picked_up = true
+	print("Health Pickup :: pickup was picked, health regained!")
 	queue_free()
