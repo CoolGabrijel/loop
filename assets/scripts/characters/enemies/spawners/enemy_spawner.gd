373ee283417a69b191a574148aea0150d144b614
CoolGabrijel <46607201+CoolGabrijel@users.noreply.github.com>
2025-08-03 15:13:54 +0200

    Added enemy variety

--- assets/scripts/characters/enemies/spawners/enemy_spawner.gd
+++ assets/scripts/characters/enemies/spawners/enemy_spawner.gd
@@ -2,6 +2,8 @@ extends Node3D
 class_name EnemySpawner
 
 const CHASER_ENEMY = preload("res://scenes/characters/enemies/chaser/chaser_enemy.tscn")
+const GUARD_ENEMY = preload("res://scenes/characters/enemies/guard/guard_enemy.tscn")
+const RANGER = preload("res://scenes/characters/enemies/ranger/ranger.tscn")
 @export var patrol_waypoints : Node3D
 
 func _ready() -> void:
@@ -9,6 +11,14 @@ func _ready() -> void:
 	pass
 
 func spawn_enemy() -> void:
-	var enemy: chaser_enemy = CHASER_ENEMY.instantiate()
+	var enemy
+	var rand = randi_range(0,2)
+	if rand == 0:
+		enemy = CHASER_ENEMY.instantiate()
+	elif rand == 1:
+		enemy = GUARD_ENEMY.instantiate()
+	elif rand == 2:
+		enemy = RANGER.instantiate()
+	
 	enemy.patrol_points = patrol_waypoints.get_path()
 	add_child(enemy)
