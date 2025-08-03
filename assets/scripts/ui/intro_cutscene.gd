05344d5b65277b10d1eb847960350a3c9d515f65
Kusaku <kusakot@gmail.com>
2025-08-03 14:53:23 +0300

    we have a cutscene now hurray

--- assets/scripts/ui/intro_cutscene.gd
+++ assets/scripts/ui/intro_cutscene.gd
@@ -2,21 +2,29 @@ extends Node2D
 
 @onready var label = $Label
 
-var intro = ["Omega Corp. They make everything and anything there, including miserable people like Jeremy.",
-"He used to be optimistic. Bright. Vibrant, even.",
-"But the corporate machine grinds everyone to dust eventually.",
-"A bogus report and a vengeful CEO later, Jeremy found himself on the street, once again looking for a new job.",
-"But every morning, he'd look out his apartment window,",
+var intro = ["Omega Corp. They make everything and anything there, including miserable people like Jeremy.", #1
+"He used to be optimistic. Bright. Vibrant, even.", # 2
+"But the corporate machine grinds everyone to dust eventually.", # 3
+"A bogus report and a vengeful CEO later, Jeremy found himself on the street, once again looking for a new job.", #4
+"But every morning, he'd look out his apartment window,", #5
 "and see the Omega Corp. building looming in the distance, towering over the rest of the city.",
 "Reminding him. Taunting him. Challenging him.",
 "No more. Today was the day.",
-"He jumped in his car and sped towards the office.",
-"When Jeremy arrived, he tightened his tie and rolled up his sleeves.",
+"He jumped in his car and sped towards the office.", 
+"When Jeremy arrived, he tightened his tie and rolled up his sleeves.", #6
 "He's only met Mr. Omega once, on his first day, and his whole life has gone downhill ever since.",
 "But they'd meet again. And again. And again.",
 "As many times as it took,  ", 
 "until Jeremy delivered his message to the ruler of the most powerful organisation in the world."]
 
+@onready var texture: TextureRect = $TextureRect
+
+# FRAMES
+const _2 = preload("res://assets/sprites/intro/2.png")
+const _3 = preload("res://assets/sprites/intro/3.png")
+const _4 = preload("res://assets/sprites/intro/4.png")
+const _5 = preload("res://assets/sprites/intro/5.png")
+const _6 = preload("res://assets/sprites/intro/6.png")
 
 var current_line = 0
 
@@ -34,6 +42,16 @@ func _input(event: InputEvent) -> void:
 
 func update_intro():
 	label.text = intro[current_line]
+	if current_line == 2:
+		texture.texture = _2
+	if current_line == 3:
+		texture.texture = _3
+	if current_line == 4:
+		texture.texture = _4
+	if current_line == 5:
+		texture.texture = _5
+	if current_line == 10:
+		texture.texture = _6
 
 
 func advance_intro():
